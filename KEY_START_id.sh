6243da98a53c6fa2f7c8fe557a1b269b6626d551
LiBingyu01 <124509769+LiBingyu01@users.noreply.github.com>
2025-09-24 23:02:35 +0800

    Update KEY_START_id.sh

--- KEY_START_id.sh
+++ KEY_START_id.sh
@@ -8,18 +8,18 @@
 #     SELECT_TYPE: "mixed" # mixed | weighted | noselect
 #     TOPN: 20 
 
-
+# $$$$$$$$$$$$$ Training
 ###### In Domain
 # ================== with opencalip convnext base ================== #
 python train_net_id.py \
   --num-gpus 4 \
   --config-file configs/convnext_base_waterovs_id.yaml \
   SOLVER.IMS_PER_BATCH 16 \
-  MODEL.GPEM.FUSION add \
+  MODEL.GPEM.FUSION alpha_fusion \
   MODEL.GPEM.DEPTHANYTHING_MODEL vitb \
   MODEL.SPIM.SELECT_TYPE "mixed" \
   MODEL.SPIM.TOPN 20 \
-  OUTPUT_DIR maris_output/0_open_convnext_base_waterovs_id_add_vitb_mixed20
+  OUTPUT_DIR maris_output/convnext_base_waterovs_id_alpha_fusion_vitb_mixed20
 
 # ================== with opencalip convnext large ================== #
 python train_net_id.py \
@@ -30,4 +30,32 @@ python train_net_id.py \
   MODEL.GPEM.DEPTHANYTHING_MODEL vitb \
   MODEL.SPIM.SELECT_TYPE "mixed" \
   MODEL.SPIM.TOPN 20 \
-  OUTPUT_DIR maris_output/0_open_convnext_large_waterovs_id_alpha_fusion_vitb_mixed20
+  OUTPUT_DIR maris_output/convnext_large_waterovs_id_alpha_fusion_vitb_mixed20
+
+# $$$$$$$$$$$$$ EVAL
+###### In Domain
+# ================== with opencalip convnext base ================== #
+python train_net_id.py \
+  --num-gpus 4 \
+  --config-file configs/convnext_base_waterovs_id.yaml \
+  --eval-only \
+  SOLVER.IMS_PER_BATCH 16 \
+  MODEL.GPEM.FUSION alpha_fusion \
+  MODEL.GPEM.DEPTHANYTHING_MODEL vitb \
+  MODEL.SPIM.SELECT_TYPE "mixed" \
+  MODEL.SPIM.TOPN 20 \
+  OUTPUT_DIR maris_output/convnext_base_waterovs_id_add_vitb_mixed20
+  MODEL.WEIGHTS PATHTOTHEWEIGHT
+
+# ================== with opencalip convnext large ================== #
+python train_net_id.py \
+  --num-gpus 4 \
+  --config-file configs/convnext_large_waterovs_id.yaml \
+  --eval-only \
+  SOLVER.IMS_PER_BATCH 16 \
+  MODEL.GPEM.FUSION alpha_fusion \
+  MODEL.GPEM.DEPTHANYTHING_MODEL vitb \
+  MODEL.SPIM.SELECT_TYPE "mixed" \
+  MODEL.SPIM.TOPN 20 \
+  OUTPUT_DIR maris_output/convnext_large_waterovs_id_alpha_fusion_vitb_mixed20
+  
